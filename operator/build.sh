2e5d73305d13d896353fd0471272038f92fef4ed
Jacek Kuśnierz <kusnierz@protonmail.com>
2024-01-09 12:57:29 +0100

    change to new

--- operator/build.sh
+++ operator/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
-docker build -t vetch/basic-kopf:tagname9 --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa_tmp)" .
-docker push vetch/basic-kopf:tagname9
+docker build -t vetch/basic-kopf:tagname10 --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa_tmp)" .
+docker push vetch/basic-kopf:tagname10
